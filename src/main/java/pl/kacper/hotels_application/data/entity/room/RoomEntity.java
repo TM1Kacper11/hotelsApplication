aebda55fe81e2c798f55d742e36d14e96dc70562
user <kacper.kowalewski@tm1.edu.pl>
2021-12-13 21:10:10 +0100

    Inishial commit

--- src/main/java/pl/kacper/hotels_application/data/entity/room/RoomEntity.java
+++ src/main/java/pl/kacper/hotels_application/data/entity/room/RoomEntity.java
@@ -22,4 +22,43 @@ public class RoomEntity {
     @ManyToOne
     @JoinColumn(name = "hotel_id", insertable = false,updatable = false)
     private HotelEntity hotel;
+
+    public RoomEntity(Integer id, BigDecimal price, Integer hotelId, HotelEntity hotel) {
+        this.id = id;
+        this.price = price;
+        this.hotelId = hotelId;
+        this.hotel = hotel;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(Integer hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public HotelEntity getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(HotelEntity hotel) {
+        this.hotel = hotel;
+    }
 }
